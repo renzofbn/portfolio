c337e113eefe3fc81eedff90c0c53365a86c5519
renzofbn <renzofbn@tutanota.com>
2025-03-19 17:19:07 -0500

    feat: add sitemap integration and update robots.txt for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import { remarkAlert } from "remark-github-blockquote-alert";
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,4 +16,7 @@ export default defineConfig({
       theme: 'vitesse-dark',
     },
   },
+
+  site: 'https://renzofbn.tech',
+  integrations: [sitemap()],
 });
